39d5a62fc2a568a812de36a7c765170b2d6519e4
aooooooqi <99944907+aooooooqi@users.noreply.github.com>
2022-02-17 23:37:47 -0700

    Add files via upload

---
+++ ElectronicScaleTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ElectronicScaleTest {
+	
+	@Test
+	public void testElectronicScale() {
+
+	}
+	@Test
+	public void testgetWeightLimit() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetCurrentWeight() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetSensitivity() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testadd() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testremove() {
+		fail("Not yet implemented");
+	}
+
+}
